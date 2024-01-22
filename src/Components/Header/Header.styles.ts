d8b37f98390174747982c0c31d278ed62b682efb
Kyle <kyle@Kyles-MacBook-Pro.local>
2024-01-22 15:26:12 +0000

    first comit

--- src/Components/Header/Header.styles.ts
+++ src/Components/Header/Header.styles.ts
@@ -121,3 +121,16 @@ export const MenuIcon = styled.div`
     font-size: 1rem;
   }
 `;
+
+export const DateRangeContainer = styled.div`
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  grid-column: span 3;
+  margin-top: 1rem;
+  @media (max-width: 768px) {
+    width: 40%;
+  }
+`;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,9 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsGlobe } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaUserCircle } from "react-icons/fa";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 import {
   StyledImage,
@@ -14,10 +17,24 @@ import {
   RightContainer,
   TextContainer,
   MenuIcon,
-  GlobeContainer,
+  DateRangeContainer,
 } from "./Header.styles";
 export const Header = () => {
-  const [searchInput, setSearchInput] = useState<String>("");
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges: any) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <StyledHeader>
       <LeftContainer>
@@ -25,7 +42,10 @@ export const Header = () => {
       </LeftContainer>
 
       <MiddleContainer>
-        <StyledInput />
+        <StyledInput
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+        />
         <StyledSearchIcon>
           <AiOutlineSearch style={{ color: "white", width: 20, height: 20 }} />
         </StyledSearchIcon>
@@ -41,6 +61,17 @@ export const Header = () => {
           <FaUserCircle />
         </MenuIcon>
       </RightContainer>
+
+      {searchInput && (
+        <DateRangeContainer>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+        </DateRangeContainer>
+      )}
     </StyledHeader>
   );
 };
